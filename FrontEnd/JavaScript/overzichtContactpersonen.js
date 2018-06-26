6c7d1698494a0fd4b5a1ebc3ff8b63be0d46883f
Max Huijsdens <m.huijsdens@gmail.com>
2018-06-26 15:31:31 +0200

    Overzichten contactpersonen en werknemers

--- FrontEnd/JavaScript/overzichtContactpersonen.js
+++ FrontEnd/JavaScript/overzichtContactpersonen.js
@@ -1,4 +1,23 @@
 $(document).ready(function(){
-
+    $.getJSON('http://localhost:8082/api/kandidaat', function(data) {
+        //alert(data);
+        $.each(data, function(key, val) {
+            
+            $('#contactpersonenTable tr:first').after(
+                '<tr>'+
+                '<td>'+val.naam+'</td>' +
+                '<td>'+val.telnr+'</td>'+
+                '<td>'+val.email+'</td>'+
+                '<td></td>'+
+                '<td>'+val.afdeling+'</td>'+
+                '</tr>'
+            );
+            /*
+            $.each(val, function(k, v) {
+                alert(k + ' --- ' + v);
+            });
+            */
+        });
+    });
 
 });
