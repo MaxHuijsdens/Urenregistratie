db7ee54c9bafa17d981719121fe4a9e9e07540c0
Max Huijsdens <m.huijsdens@gmail.com>
2018-06-26 15:14:05 +0200

    Overzichten bedrijven en kandidaten en kandidaat http path goed gezet

--- FrontEnd/JavaScript/maakKandidaat.js
+++ FrontEnd/JavaScript/maakKandidaat.js
@@ -3,10 +3,10 @@ $(document).ready(function(){
         event.preventDefault();
         var naam = $("#naamTextBox").val();
         var email = $("#emailTextBox").val();
-        var telefoon = $("#telefoonnummerTextBox").val();
-        var dataJson = '{"naam":"'+naam+'", "email":"'+email+'", "telefoon":"'+telefoon+'"}';
+        var telnr = $("#telefoonnummerTextBox").val();
+        var dataJson = '{"naam":"'+naam+'", "email":"'+email+'", "telnr":"'+telnr+'"}';
         var xhttp = new XMLHttpRequest();
-        xhttp.open("POST", "http://localhost:8082/api/testclass", true);
+        xhttp.open("POST", "http://localhost:8082/api/kandidaat", true);
         xhttp.setRequestHeader("Content-type", "application/json");
         xhttp.send(dataJson);
         alert(naam + " is toegevoegd.");
--- FrontEnd/JavaScript/overzichtBedrijven.js
+++ FrontEnd/JavaScript/overzichtBedrijven.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
-
+$.getJSON('http://localhost:8082/api/testclass', function(data) {
+    //alert(data);
+    $.each(data, function(key, val) {
+        
+        $('#bedrijvenTable tr:first').after(
+            '<tr><td>'+val.naam+'</td><td>Landsmeer 12</td></tr>'
+        );
+        /*
+        $.each(val, function(k, v) {
+            alert(k + ' --- ' + v);
+        });
+        */
+    });
+});
 
 });
