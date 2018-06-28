999fcbfa8a6146b3d175007fb7fe20586c18900b
YHulst <yasminhulst@gmail.com>
2018-06-28 11:41:52 +0200

    kandidaat endpoint toegevoegd GET by id

--- Urenregistratie/src/main/java/com/rotterdam/groep1/Urenregistratie/api/KandidaatEndpoint.java
+++ Urenregistratie/src/main/java/com/rotterdam/groep1/Urenregistratie/api/KandidaatEndpoint.java
@@ -4,9 +4,11 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -33,17 +35,17 @@ public class KandidaatEndpoint {
 	}
 	
 	
-/*	@GET
+	@GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getKandidaatById(@PathParam("id") Long id){
         if(this.kandidaatService.existsById(id)) {
-            Kandidaat employee = this.employeeWTService.findById(id);
-            return Response.ok(employee).build();
+            Kandidaat kandidaat = this.kandidaatService.findById(id);
+            return Response.ok(kandidaat).build();
         }
-        System.out.println("EmployeeWT id in GET not found!");
+        System.out.println("Kandidaat id in GET niet gevonden!");
         return Response.status(Status.NOT_FOUND).build();
-    }	*/
+    }	
 	
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
--- Urenregistratie/src/main/java/com/rotterdam/groep1/Urenregistratie/controller/KandidaatService.java
+++ Urenregistratie/src/main/java/com/rotterdam/groep1/Urenregistratie/controller/KandidaatService.java
@@ -4,7 +4,6 @@ package com.rotterdam.groep1.Urenregistratie.controller;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import com.rotterdam.groep1.Urenregistratie.domein.Kandidaat;
